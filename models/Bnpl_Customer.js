56574c24f4f0310266422380495920a7acefccc6
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-04-08 16:46:34 +0700

    Update API

--- models/Bnpl_Customer.js
+++ models/Bnpl_Customer.js
@@ -8,6 +8,6 @@ const Bnpl_PersonalSchema = new mongoose.Schema({
 });
 
 const secret = process.env.SECRET_MONGOOSE;
-Bnpl_PersonalSchema.plugin(encrypt, { secret: secret });
+Bnpl_PersonalSchema.plugin(encrypt, { secret: secret, encryptedFields: ['name', 'phone', 'citizenId', 'name_ref', 'phone_ref'] });
 
 module.exports = mongoose.model('Bnpl_Personal', Bnpl_PersonalSchema);
